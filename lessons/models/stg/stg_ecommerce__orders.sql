b2e3569f3cae6028c80bccd87130c49baa5bd739
sckintas <sckintas@gmail.com>
2025-09-18 08:17:16 +0200

    created stg and int layers

---
+++ lessons/models/stg/stg_ecommerce__orders.sql
@@ -0,0 +1,21 @@
+{{ config(
+	materialized='table'
+) }}
+
+WITH source AS (
+	SELECT *
+
+	FROM {{ source('thelook_ecommerce', 'orders') }}
+)
+
+SELECT
+	order_id,
+	user_id,
+	status,
+	created_at,
+	returned_at,
+	shipped_at,
+	delivered_at,
+	num_of_item
+
+FROM source
